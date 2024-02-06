8d927942a3150d0346d9eadab3709396c6c1f7d1
daddy-cool <metinkul@outlook.com>
2024-02-06 15:51:07 +0100

    replaced toggle-gsync.exe with forked CLI release of GsyncSwitch

--- scripts/toggle-gsync/main.lua
+++ scripts/toggle-gsync/main.lua
@@ -8,65 +8,15 @@ msg = require 'mp.msg'
 require 'mp.options'
 utils = require 'mp.utils'
 
-local statePath = mp.command_native({"expand-path", "~~/script-opts" .. "/toggle-gsync.json"})
-local exePath = mp.get_script_directory() .. "/toggle-gsync.exe"
+local exePath = mp.get_script_directory() .. "/GsyncSwitch/GsyncSwitchCli.exe"
 
 local state = {
-    gsync_active = true
+    wasDisabled = false
 }
 
 local options = {
     enabled = false
 }
-
-local function file_exists(name)
-    local f = io.open(name, "r")
-    return f ~= nil and io.close(f)
- end
- 
-
-local function loadState()
-    if not file_exists(statePath) then
-        do return end
-    end
-
-    local file = assert(io.open(statePath))
-    local json = file:read("*all")
-    file:close()
-
-    local newState = utils.parse_json(json)
-    if newState then
-        state = newState
-    else
-        msg.error("Invalid JSON format in toggle-gsync.json")
-    end
-end
-
-local function saveState()
-    local file = io.open(statePath, "w+")
-    local json, _ = utils.format_json(state)
-    file:write(json)
-    file:close()
-end
-
-local function toggleGsync()
-    local process = mp.command_native({
-        name = 'subprocess',
-        playback_only = false,
-        args = {
-            exePath
-        }
-    })
-
-    if process.status >= 0 then
-        state.gsync_active = not state.gsync_active
-        saveState()
-        -- msg.info("Toggled GSYNC active state to " .. tostring(state.gsync_active))
-    else
-        local error = process.error_string
-        msg.error('Error toggling GSYNC active state')
-    end
-end
 
 local function isEnabled()
     read_options(options, "toggle_gsync")
@@ -74,25 +24,47 @@ local function isEnabled()
 end
 
 local function disableGsync()
-    if isEnabled() == false then
+    if isEnabled() == false or state.wasDisabled == true then
         do return end
     end
 
-    loadState()
-    if state.gsync_active == true then
-        toggleGsync()
+    local process = mp.command_native({
+        name = 'subprocess',
+        playback_only = false,
+        args = {
+            exePath,
+            "disable-gsync"
+        }
+    })
+
+    if process.status >= 0 then
+        state.wasDisabled = true
         mp.commandv("seek", -5) -- workaround, somehow mpv gets stuck sometimes
+    else
+        --local error = process.error_string
+        msg.error('Error disabling GSYNC')
     end
 end
 
 local function enableGsync()
-    if isEnabled() == false then
+    if isEnabled() == false or state.wasDisabled == false then
         do return end
     end
 
-    loadState()
-    if state.gsync_active == false then
-        toggleGsync()
+    local process = mp.command_native({
+        name = 'subprocess',
+        playback_only = false,
+        args = {
+            exePath,
+            "enable-gsync"
+        }
+    })
+
+    if process.status >= 0 then
+        state.wasDisabled = false
+    else
+        --local error = process.error_string
+        msg.error('Error enabling GSYNC')
     end
 end
 
@@ -100,7 +72,15 @@ local function disable()
     mp.unregister_event(enableGsync)
 end
 
+local function verifyGsync()
+    if isEnabled() == false and state.wasDisabled == true then
+        enableGsync()
+    elseif isEnabled() == true and state.wasDisabled == false then
+        disableGsync()
+    end
+end
 
 mp.register_event("start-file", disableGsync)
 mp.register_event("shutdown", enableGsync)
 mp.register_script_message("disable", disable)
+mp.observe_property("script-opts", "string", verifyGsync)
